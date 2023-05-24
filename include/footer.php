b6bd43b945ad38e06d9dd9f282295f7526f5238e
21010-ikhsan <130653899+21010-ikhsan@users.noreply.github.com>
2023-05-24 23:04:41 +0700

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,40 @@
+      <footer class="footer">
+          <div class="container">
+
+
+              <div class="bottom-footer">
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                          <h5>Menu Pembayaran</h5>
+                          <ul>
+                              <li>
+                                  <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                              </li>
+                          </ul>
+                      </div>
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                          <h5>Alamat</h5>
+                          <p>Cikutra, Cibeunying Kidul, Bandung.</p>
+                          <h5>Phone: +62 8987 3769</a></h5>
+                      </div>
+                      <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                          <h5>Informasi Tambahan</h5>
+                          <p>Bergabunglah dengan ribuan restoran lain yang mendapatkan keuntungan karena telah bermitra dengan kami.</p>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </footer>

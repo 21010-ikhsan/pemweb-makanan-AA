608df5e7f6e2125e31b54931561b9e235d26ed7b
21010-ikhsan <130653899+21010-ikhsan@users.noreply.github.com>
2023-05-25 14:27:13 +0700

    Add files via upload

---
+++ admin/include/footer.php
@@ -0,0 +1,5 @@
+ <footer class="footer" style="text-align: center;">Code Camp BD © 2023 - Online Food Ordering System Developer - Ikhsan, Alex, Luthfy, thanks to MR HONY</a> </footer>
+ <!--/*!
+
+
+ */ -->
---
+++ admin/logout.php
@@ -0,0 +1,15 @@
+                <!-- /*!
+
+
+*/ -->
+                <?php
+session_start();
+session_destroy();
+$url = 'index.php';
+header('Location: ' . $url);
+
+?>
+                <!-- /*!
+
+
+*/ -->
